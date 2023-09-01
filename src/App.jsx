00be9ad239d19d236a6b8442eb0015068b6a92db
sween <khansween@gmail.com>
2023-09-01 23:50:22 +0530

    created added list

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,49 @@
 import React,{ useState } from 'react' 
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+function App() { 
+  
+  const [inputValue, setInputValue] = useState("")
+  const [list, setList] = useState([])
+
+
+  function handleClick(e){
+      e.preventDefault();
+      console.log(inputValue)
+      setList([...list, inputValue])
+      setInputValue("")
+  }
+
+
+  function handleDlt(e, index){
+     e.preventDefault();
+     console.log(index)
+  }
+
+
+  
+ console.log(list)
   return ( 
       <div className='container'>  
-      
+           <div>Make Your List.</div>
+           <div className='form'> 
+           <input type='text' value={inputValue} onChange={(e) => {setInputValue(e.target.value)}}></input>
+           <button onClick={e => handleClick(e)}>Add</button>
+           </div>
+           <div className='todo-list'>
+            <ul>
+
+              {
+               list &&   list.map((value, index) => {
+                    return (<li key={index}>{value}<button >Edit</button> <button onClick={e=>{handleDlt(e, index)}}>Delete</button></li> )
+                  })
+              }
+
+            </ul>
+           </div>
       </div>
-    
   )
 }
 
