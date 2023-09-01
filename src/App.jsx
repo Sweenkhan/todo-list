540b0ab2bdc26fa5b9bd4df869f7cfba8c3e0bfb
sween <khansween@gmail.com>
2023-09-02 02:40:02 +0530

    final-commit of the day

--- src/App.jsx
+++ src/App.jsx
@@ -7,36 +7,81 @@ function App() {
   
   const [inputValue, setInputValue] = useState("")
   const [list, setList] = useState([]);
-  const [edit, setEdit] = useState(false)
+  const [edit, setEdit] = useState(false);
+  const [isDone, setIsDone] = useState(false);
+  const [doneList, setDoneList] = useState([])
+  const [crntIndex, setCrntIndex] = useState(0);
 
 
+//--------------------------------------ADDING TO LIST---------------------------//
   function handleClick(e){
       e.preventDefault(); 
 
-      setList([...list, inputValue])
+      // if(edit){
+
+         let filterEdit = list.filter((li, listIndex) => {
+             return listIndex !== crntIndex
+         })
+
+      setList([...filterEdit, {item:inputValue, done:false}])
       setInputValue("")
+      //    setEdit(false)
+         
+      // }else { 
+      setList([...list, {item:inputValue, done:false}])
+      setInputValue("")
+      // }
+ 
+  }
+ 
+
+//---------------------------------EDITING LIST-------------------------------//
+  function handleEdit(e,index, li){
+     e.preventDefault()
+     console.log(li.item)
+    //  setInputValue(value)
+     setEdit(true)
+     setCrntIndex(index)
   }
 
+  
+//-------------------------------------DELETING FROM LIST-----------------------//
+function handleDlt(e, index){
+  e.preventDefault();
+  const filter = list.filter((li, listIndex) => {
+       return listIndex !== index
+  })
 
-  function handleDlt(e, index){
-     e.preventDefault();
-     const filter = list.filter((li, listIndex) => {
-          return listIndex !== index
-     })
-
-     setList(filter)
-  }
+  setList(filter)
+}
 
 
+//------------------------------------COMPLETE FROM LIST------------------------//
 
+// function handleComplete(e, index){
+//   e.preventDefault();
+//   setIsDone((index === doneList[index]) ? true : false)
+//     if(!isDone){ 
+//       setDoneList([...doneList, index])
+//       console.log("i am good")
+//     }
+//     else{
+     
+//       const filterCrossLine = list.filter((li, listIndex) => {
+//           return listIndex !== index
+//       })
+// console.log(filterCrossLine)
+// console.log("how are you")
+//       setDoneList(filterCrossLine)
+//     }
 
-  function handleEdit(){
+// } 
 
-  }
 
 
 
  console.log(list)
+
   return ( 
       <div className='container'>  
            <div>Make Your List.</div>
@@ -44,21 +89,40 @@ function App() {
            <input type='text' value={inputValue} onChange={(e) => {setInputValue(e.target.value)}}></input>
            <button onClick={e => handleClick(e)}>Add</button>
            </div>
-           <div className='todo-list'>
+
+           <div className='todos'>
             <ul>
+              {
+                 list.map((li, index) => {
+                    return (
+                      <li key={index}>{li.item}
+                      <button onClick={(e) => handleEdit(e, index, li)}>Edit</button> 
+                      <button onClick={e=>{handleDlt(e, index)}}>Delete</button>
+
+                      </li>
+                    )
+                 })
+              }
+            </ul>
+           </div>
+
+           <div className='todo-list'>
+            {/* <ul>
 
               {
                list &&   list.map((value, index) => {
                     return (
-                      <li key={index}>{value}
-                    <button onClick={(e) => handleEdit(e, index)}>Edit</button> 
+                      <li key={index}  
+                      style={{textDecoration: index === doneList[index] ?  'line-through' : 'none'}}>{value}
+                    <button onClick={(e) => handleEdit(e, value, index)}>Edit</button> 
                     <button onClick={e=>{handleDlt(e, index)}}>Delete</button>
+                    <button onClick={(e) => {handleComplete(e, index)}}>{ (index === doneList[index]) ? "Undo": "Done"}</button>
                     </li> 
                     )
                   })
               }
 
-            </ul>
+            </ul> */}
            </div>
       </div>
   )
